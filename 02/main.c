8dd4c0ea41b62a3e3a0796c6ce6bd1793af73e84
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-17 15:10:26 +0100

    debugando codigos

---
+++ 02/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("Mensagem 0\n");
+	printf("Mensagem 1\n");
+	printf("Mensagem 2\n");
+	printf("Mensagem 3\n");
+	printf("Mensagem 4\n");
+	
+	return 0;
+}
