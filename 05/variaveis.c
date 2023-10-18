18d548af4ef4a5bdc90688960e53b28018cec3d5
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-18 17:37:51 +0100

    variaveis

--- 05/variaveis.c
+++ 05/variaveis.c
@@ -1,9 +1,11 @@
 # include <stdio.h>
 
-#define texto "Entrada e saida e dados"
+#define texto "Entrada e saida de dados"
 
 int main() {
+	printf("###############################\n");
 	printf("%s\n", texto);
+	printf("###############################\n");
 	
 	int idade = 0;
 	float altura = 0.0;
@@ -18,6 +20,12 @@ int main() {
 	printf("Digite o nome:");
 	scanf("%s", &nome);
 	
-	printf("%s voce tem %d anos de idades, com uma altura a rondar a %.2f metros", nome, idade, altura);
+	printf("###############################\n");
+	printf("Dados informados:\n");
+	printf("Nome: %s\n", nome);
+	printf("Idade: %d\n", idade);
+	printf("Altura: %.2f\n", altura);
+	printf("###############################\n");
+	// printf("%s voce tem %d anos de idades, com uma altura a rondar a %.2f metros", nome, idade, altura);
 	
 }
