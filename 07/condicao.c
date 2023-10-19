79d8f9ad69d746aa7c662137548d7fed0b752adc
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-19 18:47:44 +0100

    condicoes
    rectificada o nome do programa e criada lacos no meu programa

--- 07/condicao.c
+++ 07/condicao.c
@@ -7,7 +7,13 @@ int main() {
 	printf("Insira a nota:");
 	scanf("%f", &m);
 	
-	if(m > 7.0){
+	if(m >= 7.0){
 		printf("Aprovado(a)!\n");
+	} else {
+		if (m > 4) {
+			printf("Foste ao recurso");
+		} else {
+			printf("Reprovado(a)");
+		}
 	}
 }
