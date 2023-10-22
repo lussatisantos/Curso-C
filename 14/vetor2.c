95535f607d3508a077a140d5c1cb4fd9b9c901d7
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-22 18:18:44 +0100

    vetor2

---
+++ 14/vetor2.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main() {
+	
+	int v[5];
+	
+	float m;
+	
+	v[0] = 50;
+	v[1] = 40;
+	v[2] = 30;
+	v[3] = 20;
+	v[4] = 10;
+	
+	m = (v[0] + v[1] + v[2] + v[3] + v[4]) / 5;
+	
+	printf("Resultado: %.0f\n", m);
+		
+}
+
