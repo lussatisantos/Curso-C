d8bae14545efd6aca05ee59e0232773707f2cc2e
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-23 17:33:27 +0100

    strings

---
+++ 15/string.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main() {
+	char s[10];
+	
+	printf("Digite algo (scanf convencional): ");
+	scanf("%s", s);
+	fflush(stdin);
+	printf("Resultado: %s\n\n", s);
+	
+	printf("Digite algo (scanf aprimorado): ");
+	scanf("%10[^\n]s", s);
+	fflush(stdin);
+	printf("Resultado: %s\n\n", s);
+}
